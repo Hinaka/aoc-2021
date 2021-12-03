e45cc10078466df8f51e82f7a9380dbc316665ca
Hinaka <76215941+Hinaka@users.noreply.github.com>
2021-12-04 00:42:39 +0700

    Day 03 update algorithm

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,28 +1,31 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        val inputTable = input.map { line -> line.map { it.digitToInt() } }
-        val bitsSumIndexed = inputTable.bitsSumIndexed()
+        val bitOccurrenceByIndex = input.first().indices.map { index ->
+            input.bitOccurrenceAt(index)
+        }
 
-        val gamma = bitsSumIndexed
-            .map { bit -> if (bit > 0) 1 else 0 }
-            .joinToString("")
-            .toInt(2)
-        val epsilon = bitsSumIndexed
-            .map { bit -> if (bit > 0) 0 else 1 }
-            .joinToString("")
-            .toInt(2)
+        val mostCommonBits = bitOccurrenceByIndex.joinToString("") { bitOccurrence ->
+            bitOccurrence.maxByOrNull { it.value }?.key.toString()
+        }
+
+        val leastCommonBits = bitOccurrenceByIndex.joinToString("") { bitOccurrence ->
+            bitOccurrence.minByOrNull { it.value }?.key.toString()
+        }
+
+        val gamma = mostCommonBits.toInt(2)
+        val epsilon = leastCommonBits.toInt(2)
 
         return gamma * epsilon
     }
 
     fun part2(input: List<String>): Int {
-        val inputTable = input.map { line -> line.map { it.digitToInt() } }
-        val oxygenGeneratorRating = inputTable.getRating { bitsSum ->
-            if (bitsSum >= 0) 1 else 0
-        }
-        val co2ScrubberRating = inputTable.getRating { bitsSum ->
-            if (bitsSum >= 0) 0 else 1
-        }
+        val oxygenGeneratorRating = input.findRating { zeroes, ones ->
+            if (ones >= zeroes) '1' else '0'
+        }.toInt(2)
+
+        val co2ScrubberRating = input.findRating { zeroes, ones ->
+            if (ones >= zeroes) '0' else '1'
+        }.toInt(2)
 
         return oxygenGeneratorRating * co2ScrubberRating
     }
@@ -36,31 +39,17 @@ fun main() {
     println(part2(input))
 }
 
-/**
- * Increase by 1 if bit is 1, and decrease by 1 if bit is 0. Hence, a positive value indicates more 1 bits
- * at that index, 0 means equals, and a negative value indicates more 0 bits.
- */
-private fun List<List<Int>>.bitsSumIndexed() = first().indices
-    .map { index ->
-        calculateBitsSumAt(index)
-    }
+private fun List<String>.bitOccurrenceAt(index: Int) = groupingBy { it[index] }.eachCount()
 
-private fun List<List<Int>>.calculateBitsSumAt(index: Int) =
-    fold(0) { occurrences, bits ->
-        if (bits[index] == 1) {
-            occurrences + 1
-        } else {
-            occurrences - 1
-        }
+private fun List<String>.findRating(getCriteria: (zeroes: Int, ones: Int) -> Char): String {
+    val validInputs = toMutableList()
+    for (index in first().indices) {
+        val bitOccurrences = validInputs.bitOccurrenceAt(index)
+        val zeroes = bitOccurrences['0'] ?: 0
+        val ones = bitOccurrences['1'] ?: 0
+        val bitCriteria = getCriteria(zeroes, ones)
+        validInputs.removeAll { it[index] != bitCriteria }
+        if (validInputs.size == 1) break
     }
-
-private fun List<List<Int>>.getRating(index: Int = 0, createBitCriteria: (bitsSum: Int) -> Int): Int {
-    val bitsSum = calculateBitsSumAt(index)
-    val bitCriteria = createBitCriteria(bitsSum)
-    val validInputs = filter { input -> input[index] == bitCriteria }
-    return if (validInputs.size == 1) {
-        validInputs.first().joinToString("").toInt(2)
-    } else {
-        validInputs.getRating(index +1, createBitCriteria)
-    }
+    return validInputs.single()
 }
