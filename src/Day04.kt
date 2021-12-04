04e4d637dcb64dc8b50b7083747011157b7d70ac
Hinaka <76215941+Hinaka@users.noreply.github.com>
2021-12-04 15:46:12 +0700

    Day 04 part 1

---
+++ src/Day04.kt
@@ -0,0 +1,85 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        val draws = input.getDraws()
+        val boards = input.getBoards().map { Board.from(it) }
+
+        for (currentDraw in draws) {
+            for (board in boards) {
+                board.mark(currentDraw)
+                if (board.isBingo()) {
+                    val unmarkedSum = board.unmarkedFields().sumOf { it.value }
+                    return unmarkedSum * currentDraw
+                }
+            }
+        }
+
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    val testInput = readInput("Day04_test")
+    check(part1(testInput) == 4512)
+//    check(part2(testInput) == 230)
+//
+    val input = readInput("Day04")
+    println(part1(input))
+//    println(part2(input))
+}
+
+private fun List<String>.getDraws() = first().split(",").map { it.toInt() }
+
+private fun List<String>.getBoards() = drop(1)
+    .chunked(6)
+    .map { lines ->
+        lines.filter { line -> line.isNotBlank() }
+    }.map { board ->
+        board.map { line ->
+            line.split(" ")
+                .filter { it.isNotBlank() }
+                .map { it.trim().toInt() }
+        }
+    }
+
+data class Field(val value: Int, val isMarked: Boolean = false)
+
+data class Board(val fields: List<MutableList<Field>>) {
+
+    private val indices = fields.indices
+
+    fun mark(value: Int) {
+        for (row in fields) {
+            row.replaceAll { field ->
+                if (field.value == value) field.copy(isMarked = true) else field
+            }
+        }
+    }
+
+    fun isBingo() = isRowBingo() || isColumnBingo()
+
+    private fun isRowBingo() = fields.any { row -> row.all { it.isMarked } }
+
+    private fun isColumnBingo() : Boolean {
+        for (col in indices) {
+            var bingo = true
+            for (row in indices) {
+                if (!fields[row][col].isMarked) {
+                    bingo = false
+                    break
+                }
+            }
+            if (bingo) return true
+        }
+        return false
+    }
+
+    fun unmarkedFields() = fields.flatten().filter { !it.isMarked }
+
+    companion object {
+        fun from(input: List<List<Int>>) = Board(
+            input.map { line -> line.map { Field(it) }.toMutableList() }
+        )
+    }
+}
