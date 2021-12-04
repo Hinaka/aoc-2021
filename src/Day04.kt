6637912cd300e8eb80c4f45cb9e8ec53855016d5
Hinaka <76215941+Hinaka@users.noreply.github.com>
2021-12-04 16:22:30 +0700

    Day 04 part 2

--- src/Day04.kt
+++ src/Day04.kt
@@ -17,16 +17,35 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val draws = input.getDraws()
+        val boards = input.getBoards().map { Board.from(it) }.toMutableList()
+
+        for (currentDraw in draws) {
+            val iterator = boards.iterator()
+            while (iterator.hasNext()) {
+                val board = iterator.next()
+                board.mark(currentDraw)
+                if (board.isBingo()) {
+                    if (boards.size == 1) {
+                        val unmarkedSum = board.unmarkedFields().sumOf { it.value }
+                        return unmarkedSum * currentDraw
+                    } else {
+                        iterator.remove()
+                    }
+                }
+            }
+        }
+
+        return 0
     }
 
     val testInput = readInput("Day04_test")
     check(part1(testInput) == 4512)
-//    check(part2(testInput) == 230)
-//
+    check(part2(testInput) == 1924)
+
     val input = readInput("Day04")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
 
 private fun List<String>.getDraws() = first().split(",").map { it.toInt() }
@@ -61,7 +80,7 @@ data class Board(val fields: List<MutableList<Field>>) {
 
     private fun isRowBingo() = fields.any { row -> row.all { it.isMarked } }
 
-    private fun isColumnBingo() : Boolean {
+    private fun isColumnBingo(): Boolean {
         for (col in indices) {
             var bingo = true
             for (row in indices) {
