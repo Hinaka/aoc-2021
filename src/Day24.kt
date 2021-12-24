298ca044447942a913a266325123490f633f59c6
Hinaka <76215941+Hinaka@users.noreply.github.com>
2021-12-24 21:41:09 +0700

    Day 24 solved by hand

---
+++ src/Day24.kt
@@ -0,0 +1,15 @@
+fun main() {
+    // Solved both by hand using the idea from this https://github.com/dphilipson/advent-of-code-2021/blob/master/src/days/day24.rs
+
+    fun part1(input: List<String>): Long {
+        return 53999995829399
+    }
+
+    fun part2(input: List<String>): Long {
+        return 11721151118175 //solved by hand
+    }
+
+    val input = readInput("Day24.txt")
+    println(part1(input)) //
+    println(part2(input))
+}
